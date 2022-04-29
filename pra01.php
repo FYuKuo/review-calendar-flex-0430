08368dabc2a356f4c3e0814561615e6ed20fc746
FY Kuo <teresa99339@gmail.com>
2022-04-30 00:20:34 +0800

    將日期的格式改為日,並加入註解

--- pra01.php
+++ pra01.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,6 +11,7 @@
             box-sizing: border-box;
 
         }
+
         .table {
             display: flex;
             flex-wrap: wrap;
@@ -17,29 +19,31 @@
             height: 700px;
             align-content: flex-start;
             text-align: center;
-            
+
         }
+
         .table>div {
             width: 100px;
             height: 100px;
             border: 1px solid lightblue;
             box-sizing: border-box;
-            
+
 
         }
     </style>
 </head>
+
 <body>
     <h2>利用Flex模式+陣列製作月曆</h2>
     <?php
-    $month=5; //月份
-    $firstDay=date("Y-") . $month . "-1"; //月份的第一天日期
-    $firstDaySecond=strtotime($firstDay); //月份的第一天秒數
-    $firstDayWeek=date("w",$firstDaySecond); //月份的第一天星期幾
-    $monthDay=date("t",$firstDaySecond); //月份有幾天
-    $lastDay=date("Y-") . $month . "-" . $monthDay; //月份最後一天日期
-    $lastDaySecond=strtotime($lastDay); //月份最後一天秒數
-    $lastDayWeek=date("w",$lastDaySecond); //月份最後一天星期幾
+    $month = 5; //月份
+    $firstDay = date("Y-") . $month . "-1"; //月份的第一天日期
+    $firstDaySecond = strtotime($firstDay); //月份的第一天秒數
+    $firstDayWeek = date("w", $firstDaySecond); //月份的第一天星期幾
+    $monthDay = date("t", $firstDaySecond); //月份有幾天
+    $lastDay = date("Y-") . $month . "-" . $monthDay; //月份最後一天日期
+    $lastDaySecond = strtotime($lastDay); //月份最後一天秒數
+    $lastDayWeek = date("w", $lastDaySecond); //月份最後一天星期幾
 
     echo "月份 ==> $month <br>";
     echo "月份的第一天日期 ==> $firstDay <br>";
@@ -52,19 +56,22 @@
 
     echo "<hr>";
 
-    $allDays=[]; //所有天數的空陣列
+    $allDays = []; //所有天數的空陣列
 
-    for($i=0; $i<$firstDayWeek ; $i++){
-        $allDays[]="";
+    //將月份開始前的空白加入陣列
+    for ($i = 0; $i < $firstDayWeek; $i++) {
+        $allDays[] = "";
     }
 
-    for($i=0; $i<$monthDay; $i++){
-        $date=date("Y-m-d",strtotime("+$i days",$firstDaySecond));
-        $allDays[]=$date;
+    //月份所有的日期加入陣列
+    for ($i = 0; $i < $monthDay; $i++) {
+        $date = date("Y-m-d", strtotime("+$i days", $firstDaySecond));
+        $allDays[] = $date;
     }
 
-    for($i=0; $i<6-$lastDayWeek; $i++){
-        $allDays[]="";
+    //月份最後的空白加入陣列
+    for ($i = 0; $i < 6 - $lastDayWeek; $i++) {
+        $allDays[] = "";
     }
 
     echo "<pre>";
@@ -76,28 +83,44 @@
     ?>
 
     <div class="table">
-    <div class="header">星期日</div>
-    <div class="header">星期一</div>
-    <div class="header">星期二</div>
-    <div class="header">星期三</div>
-    <div class="header">星期四</div>
-    <div class="header">星期五</div>
-    <div class="header">星期六</div>
-    <?php
 
-    foreach($allDays as $day){
-        echo "<div> {$day} </div>";
-    }
+        <div class="header">星期日</div>
+        <div class="header">星期一</div>
+        <div class="header">星期二</div>
+        <div class="header">星期三</div>
+        <div class="header">星期四</div>
+        <div class="header">星期五</div>
+        <div class="header">星期六</div>
+        
+        <?php
 
 
+        //利用陣列的FOR迴圈印出表格及日期
+        foreach ($allDays as $day) {
+            
+            //如果$day字串不是空白時轉換字串的型態為日期,並印出div和日期
+            if (!empty($day)) { 
+
+                $dateFont = date("d", strtotime($day));
+                echo "<div> {$dateFont} </div>";
+                
+            }else{
+                //如果$day字串是空白時,印出空白的div
+                echo "<div></div>";
+            }
+            
+        }
 
 
 
-    ?>
+
+
+        ?>
 
     </div>
 
 
 
 </body>
+
 </html>
