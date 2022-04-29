2df835b5d2445179048ced8a9e44e9d68fcfbc67
FY Kuo <teresa99339@gmail.com>
2022-04-29 19:48:32 +0800

    建立月曆所需的變數 並印出檢查正確

--- pra01.php
+++ pra01.php
@@ -5,8 +5,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>課後複習-0429</title>
+    <style>
+
+    </style>
 </head>
 <body>
-    
+    <h2>利用Flex模式+陣列製作月曆</h2>
+    <?php
+    $month=4; //月份
+    $firstDay=date("Y-") . $month . "-1"; //月份的第一天日期
+    $firstDaySecond=strtotime($firstDay); //月份的第一天秒數
+    $firstDayWeek=date("w",$firstDaySecond); //月份的第一天星期幾
+    $monthDay=date("t",$firstDaySecond); //月份有幾天
+    $lastDay=date("Y-") . $month . "-" . $monthDay; //月份最後一天日期
+    $lastDaySecond=strtotime($lastDay); //月份最後一天秒數
+    $lastDayWeek=date("w",$lastDaySecond); //月份最後一天星期幾
+
+    echo "月份 ==> $month <br>";
+    echo "月份的第一天日期 ==> $firstDay <br>";
+    echo "月份的第一天秒數 ==> $firstDaySecond <br>";
+    echo "月份的第一天星期幾 ==> $firstDayWeek <br>";
+    echo "月份有幾天 ==> $monthDay <br>";
+    echo "月份最後一天日期 ==> $lastDay <br>";
+    echo "月份最後一天秒數 ==> $lastDaySecond <br>";
+    echo "月份最後一天星期幾 ==> $lastDayWeek <br>";
+
+    echo "<hr>";
+
+
+
+
+
+
+
+
+
+
+    ?>
+
 </body>
 </html>
